41a84eb5c9220876e79f7e2dd600740011d8d9f5
texno stalin <texno.stalin@gmail.com>
2026-01-26 16:35:30 +0300

    feat(minio): добавить модель настроек MinioOptions с валидацией

---
+++ dotnet/Minerva/Minerva/Common/Minio/MinioOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Minerva.Common.Minio
+{
+    public class MinioOptions
+    {
+        public const string SectionName = "Minio";
+
+        [Required, Url]
+        public string Endpoint { get; set; } = string.Empty;
+
+        [Required]
+        public string AccessKey { get; set; } = string.Empty;
+
+        [Required]
+        public string SecretKey { get; set; } = string.Empty;
+
+        [Required]
+        public string BucketName { get; set; } = string.Empty;
+
+        public bool UseSSL { get; set; } = false;
+
+        // Можно добавить регион, если планируете переезд в AWS S3
+        public string Region { get; set; } = "us-east-1";
+    }
+}

3dcb2ed7ca1b6b695367bbf99b678381f9d9aa17
texno stalin <texno.stalin@gmail.com>
2026-01-26 16:40:01 +0300

    feat(project): инициализировать базовую структуру и инфраструктуру проекта
    - Настроены файлы конфигурации appsettings.json и launchSettings.json
    - Добавлена поддержка Docker (Dockerfile и .dockerignore)
    - Создан файл проекта .csproj и основная точка входа Program.cs
    - Добавлен файл Minerva.http для тестирования API

---
+++ dotnet/Minerva/Minerva/Program.cs
@@ -0,0 +1,62 @@
+using Minerva.Common.Minio;
+using Minerva.Features.DeleteFile;
+using Minerva.Features.GetFile;
+using Minerva.Features.UploadFile;
+using Minio;
+
+namespace Minerva
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var minioConfig = builder.Configuration.GetSection("Minio");
+            var endpoint = minioConfig["Endpoint"];
+            var accessKey = minioConfig["AccessKey"];
+            var secretKey = minioConfig["SecretKey"];
+            var useSsl = bool.Parse(minioConfig["UseSsl"] ?? "false");
+
+            // TODO : ѕока не используетс€, но потом может пригодитьс€
+            builder.Services.AddOptions<MinioOptions>()
+                .Bind(builder.Configuration.GetSection(MinioOptions.SectionName));
+
+            builder.Services.AddSingleton<IMinioClient>(sp =>
+            {
+                return new MinioClient()
+                    .WithEndpoint(endpoint)
+                    .WithCredentials(accessKey, secretKey)
+                    .WithSSL(useSsl)
+                    .Build();
+            });
+
+            builder.Services.AddMediatR(cfg => 
+                cfg.RegisterServicesFromAssembly(typeof(Program).Assembly)
+            );
+
+            var app = builder.Build();
+
+            app.MapUploadFile();
+            app.MapGetFile();
+            app.MapDeleteFile();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
